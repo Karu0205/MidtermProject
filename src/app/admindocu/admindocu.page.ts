44f123acaa6a1bb97901b122b8df162a39d600e4
Karu0205 <miguel0205.cmp@gmail.com>
2023-07-09 19:11:13 +0800

    interface update

--- src/app/admindocu/admindocu.page.ts
+++ src/app/admindocu/admindocu.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../service/firebase.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-admindocu',
@@ -9,7 +10,7 @@ import { FirebaseService } from '../service/firebase.service';
 export class AdmindocuPage implements OnInit {
   accounts = [] as any;
 
-  constructor(private dataService: FirebaseService) {
+  constructor(private dataService: FirebaseService, private alertCtrl: AlertController) {
     this.dataService.getAccounts().subscribe(res => {
       console.log(res);
       this.accounts=res;
@@ -20,7 +21,34 @@ export class AdmindocuPage implements OnInit {
   }
 
   accountList(){
-    
+
+  }
+
+  async addAccount(){
+    const alert = await this.alertCtrl.create({
+      header: 'Add Account',
+      inputs: [
+        {
+          name: 'Student Id',
+          placeholder: 'Enter Id',
+          type: 'text'
+        },
+
+        {
+          name:'Name of Student ',
+          placeholder: 'Enter Name Of student',
+          type: 'text'
+        },
+        {
+          name: 'Password of Student',
+          placeholder: 'Enter student password',
+          type: 'text'
+        }
+      ],
+      buttons:[
+        
+      ]
+    })
   }
 
 }
